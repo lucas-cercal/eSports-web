e9e59f3f95d04f4145c7fb847c8dd0c285e709df
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-10 12:06:29 -0300

    Personalizando página inicial

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
     extend: {
-      
+      backgroundImage: {
+        galaxy: 'url("/background-galaxy.png")',
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 48.57%)',
+      }
     },
   },
   plugins: [],
