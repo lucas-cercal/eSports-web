a6a94b9029c270809aad938e754f5fd85deceb48
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-10 16:26:04 -0300

    Finalizando CSS da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -17,42 +17,42 @@ function App() {
           <img src="/game-1.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>6 Anúncios</span>
           </div>
         </a>
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-2.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Dota 2</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>5 Anúncios</span>
           </div>
         </a>
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-3.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Counter Strike</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>8 Anúncios</span>
           </div>
         </a>
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-4.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Apex Legends</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>3 Anúncios</span>
           </div>
         </a>
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-5.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>Fortnite</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>3 Anúncios</span>
           </div>
         </a>
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-6.png" alt="" />
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>World of Warcraft</strong>
-            <span className='text-zinc-300 text-sm mt-1'>4 Anúncios</span>
+            <span className='text-zinc-300 text-sm mt-1'>2 Anúncios</span>
           </div>
         </a>
       </div>
