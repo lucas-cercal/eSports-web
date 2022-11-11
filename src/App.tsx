8fd48c982fbda6480f87a4ef7fc7cac6f41f139c
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 20:02:12 -0300

    Renderizando games do back-end na home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
 
@@ -5,7 +7,27 @@ import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
 
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number
+  }
+}
+
 function App() {
+
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} />
@@ -15,15 +37,20 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner 
-          bannerUrl='/game-1.png'
-          title='League of Legends'
-          adsCount={5}
-        />
+        {games.map(game => {
+          return (
+            <GameBanner
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
+
       </div>
 
       <CreateAdBanner />
-      
+
     </div>
   )
 }
